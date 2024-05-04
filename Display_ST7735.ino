2b91937883c35eb36e1285d4a8f28745b2520ab2
Rob Antonisse <robantonisse@gmail.com>
2024-05-04 10:31:15 +0200

    Add project files.

---
+++ Display_ST7735.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Display_ST7735.ino
+ Created:	5/4/2024 10:30:09 AM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
