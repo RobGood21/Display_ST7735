e1a9df6950a78549da40ebced041f2d53ce354d8
Rob Antonisse <robantonisse@gmail.com>
2024-05-24 17:20:16 +0200

    start oled tester

---
+++ DisplayOledTester/DisplayOledTester.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		DisplayOledTester.ino
+ Created:	5/24/2024 5:18:04 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
